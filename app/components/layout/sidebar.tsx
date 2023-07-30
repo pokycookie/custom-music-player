28435f4979ca2df130e482e1ed686f30d67bd86e
cookie <0cookieboy0@gmail.com>
2023-07-30 19:42:31 +0900

    feat: sidebar

--- app/components/layout/sidebar.tsx
+++ app/components/layout/sidebar.tsx
@@ -1,3 +1,86 @@
+'use client'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faHouse,
+  faHeadphones,
+  faMusic,
+  faGear,
+} from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
+import { motion } from 'framer-motion'
+
 export default function Sidebar() {
-  return <nav className="w-56 h-full shrink-0 bg-zinc-800"></nav>;
+  const [pageIdx, setPageIdx] = useState(0)
+  const [hoverIdx, setHoverIdx] = useState<number | null>(null)
+
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setPageIdx(() => {
+      const idx = navList.findIndex((e) => e.regex.test(pathname))
+      if (idx > -1) return idx
+      return 0
+    })
+  }, [pathname])
+
+  return (
+    <nav className="flex flex-col justify-between w-56 h-full shrink-0 bg-zinc-800">
+      <div>
+        <h1></h1>
+        <ul className="relative">
+          <motion.div
+            className="absolute w-1 bg-purple-600 h-14"
+            animate={{ y: 56 * (hoverIdx ?? pageIdx) }}
+          ></motion.div>
+          {navList.map((e, i) => {
+            return (
+              <li
+                key={i}
+                onMouseEnter={() => setHoverIdx(i)}
+                onMouseLeave={() => setHoverIdx(null)}
+              >
+                <Link
+                  href={e.href}
+                  className="flex items-center gap-4 p-4 pl-5 text-gray-200 h-14"
+                >
+                  <FontAwesomeIcon icon={e.icon} />
+                  <p>{e.title}</p>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <div className="w-full p-3">
+        <button className="flex items-center justify-center w-full gap-3 p-3 font-semibold text-gray-200 rounded bg-zinc-600 hover:bg-zinc-500">
+          <FontAwesomeIcon icon={faMusic} />
+          <p>노래 추가</p>
+        </button>
+      </div>
+    </nav>
+  )
 }
+
+const navList = [
+  {
+    title: '홈',
+    icon: faHouse,
+    href: '/',
+    regex: /^\/$/,
+  },
+  {
+    title: '라이브러리',
+    icon: faHeadphones,
+    href: '/library',
+    regex: /^\/library$/,
+  },
+  {
+    title: '설정',
+    icon: faGear,
+    href: '/setting',
+    regex: /^\/setting$/,
+  },
+]
