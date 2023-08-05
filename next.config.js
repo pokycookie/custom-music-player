52add40a36af40c052af2d2e021b21bd42d6fe85
cookie <0cookieboy0@gmail.com>
2023-08-05 18:25:47 +0900

    feat: icon, svgr, set webpack

--- next.config.js
+++ next.config.js
@@ -3,6 +3,25 @@ const nextConfig = {
   images: {
     domains: ['i.ytimg.com'],
   },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    )
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        use: ['@svgr/webpack'],
+      }
+    )
+    return config
+  },
 }
 
 module.exports = nextConfig
