f8ad3eaddf1da8e841ea6ebd8e7f50d4e4a605b5
cookie <0cookieboy0@gmail.com>
2023-08-07 13:01:08 +0900

    fix: empty carousel bug

--- app/components/ui/carousel.tsx
+++ app/components/ui/carousel.tsx
@@ -85,7 +85,7 @@ export default function Carousel(props: IProps) {
             )
           })}
         </div>
-        {!props.infinite && idx != 0 ? (
+        {!props.infinite && container.length > 1 && idx != 0 ? (
           <button
             onClick={() => indexHandler(-1)}
             className="absolute flex items-center justify-center w-12 h-12 text-gray-400 duration-200 bg-opacity-50 border rounded-full border-zinc-600 bg-zinc-900 hover:bg-opacity-100 -left-6 hover:scale-105"
@@ -93,7 +93,9 @@ export default function Carousel(props: IProps) {
             <FontAwesomeIcon icon={faChevronLeft} className="w-3 h-3" />
           </button>
         ) : null}
-        {!props.infinite && idx != container.length - 1 ? (
+        {!props.infinite &&
+        container.length > 1 &&
+        idx != container.length - 1 ? (
           <button
             onClick={() => indexHandler(1)}
             className="absolute flex items-center justify-center w-12 h-12 text-gray-400 duration-200 bg-opacity-50 border rounded-full border-zinc-600 bg-zinc-900 hover:bg-opacity-100 -right-6 hover:scale-105"
