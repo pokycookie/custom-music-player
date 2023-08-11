e8b86a07da97995d4fa518cfac3328e73f76d44f
cookie <0cookieboy0@gmail.com>
2023-08-11 23:44:49 +0900

    fix: overflow hidden bug

--- app/components/modal/export.tsx
+++ app/components/modal/export.tsx
@@ -7,6 +7,7 @@ import useAllCheck from '@/hooks/useAllCheck'
 import Check from '../ui/check'
 import { useLiveQuery } from 'dexie-react-hooks'
 import db, { IDBMusic, IDBPlaylist } from '@/db'
+import { motion } from 'framer-motion'
 
 interface IProps {
   close: () => void
@@ -41,33 +42,41 @@ export default function ExportModal(props: IProps) {
         title="export music"
         onChange={() => setMusicOption((prev) => !prev)}
       >
-        <div className="flex items-center gap-3 p-2 pb-3 mb-2 border-b border-zinc-600">
-          <Check
-            checked={musicChecks.allCheck}
-            onChange={(checked) =>
-              musicChecks.allCheckHandler(checked, musics?.length ?? 0)
-            }
-          />
+        <div className="h-full overflow-hidden">
+          <div className="flex items-center justify-between gap-3 p-2 pb-3 mb-2 border-b border-zinc-600">
+            <Check
+              checked={musicChecks.allCheck}
+              onChange={(checked) =>
+                musicChecks.allCheckHandler(checked, musics?.length ?? 0)
+              }
+            />
+            <span className="flex items-center gap-2 select-none">
+              <p className="font-semibold text-purple-600">
+                {musicChecks.checks.size}
+              </p>
+              <p className="text-sm text-zinc-500">selected</p>
+            </span>
+          </div>
+          <ul className="pl-2 pr-4 overflow-y-auto max-h-56">
+            {musics?.map((music, i) => {
+              return (
+                <li
+                  key={i}
+                  className="flex items-center gap-3 pt-1 pb-1 text-sm text-zinc-500"
+                >
+                  <Check
+                    checked={musicChecks.checks.has(i)}
+                    onChange={(checked) => musicChecks.checkHandler(i, checked)}
+                  />
+                  <span className="flex items-center justify-between w-full">
+                    <p className="text-zinc-400">{music.title}</p>
+                    <p className="text-xs">{music.artist}</p>
+                  </span>
+                </li>
+              )
+            }) ?? null}
+          </ul>
         </div>
-        <ul className="pl-2 pr-2 overflow-y-auto max-h-56">
-          {musics?.map((music, i) => {
-            return (
-              <li
-                key={i}
-                className="flex items-center gap-3 pt-1 pb-1 text-sm text-zinc-500"
-              >
-                <Check
-                  checked={musicChecks.checks.has(i)}
-                  onChange={(checked) => musicChecks.checkHandler(i, checked)}
-                />
-                <span className="flex items-center justify-between w-full">
-                  <p className="text-zinc-400">{music.title}</p>
-                  <p className="text-xs">{music.artist}</p>
-                </span>
-              </li>
-            )
-          }) ?? null}
-        </ul>
       </ToggleSection>
       <ToggleSection
         open={playlistOptions}
