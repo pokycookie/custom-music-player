cc0766a2e5afd6b4d97489db2c97666cccbcc1fe
pokycookie <0cookieboy0@gmail.com>
2023-08-23 14:48:00 +0900

    feat: sidebar current idx color

--- app/components/layout/sidebar.tsx
+++ app/components/layout/sidebar.tsx
@@ -15,6 +15,7 @@ import { usePathname } from 'next/navigation'
 import { motion } from 'framer-motion'
 import useModal from '@/hooks/useModal'
 import AddMusic from '../modal/addMusic'
+import styled from '@emotion/styled'
 
 export default function Sidebar() {
   const [pageIdx, setPageIdx] = useState(0)
@@ -42,32 +43,30 @@ export default function Sidebar() {
 
   return (
     <nav className="flex flex-col justify-between w-56 h-full shrink-0 bg-zinc-800">
-      <div>
-        <h1></h1>
-        <ul className="relative">
-          <motion.div
-            className="absolute w-1 bg-purple-500 h-14"
-            animate={{ y: 56 * (hoverIdx ?? pageIdx) }}
-          ></motion.div>
-          {navList.map((e, i) => {
-            return (
-              <li
-                key={i}
-                onMouseEnter={() => setHoverIdx(i)}
-                onMouseLeave={() => setHoverIdx(null)}
+      <ul className="relative">
+        <motion.div
+          className="absolute w-1 bg-purple-400 h-14"
+          animate={{ y: 56 * (hoverIdx ?? pageIdx) }}
+        ></motion.div>
+        {navList.map((e, i) => {
+          return (
+            <List
+              key={i}
+              selected={pageIdx === i}
+              onMouseEnter={() => setHoverIdx(i)}
+              onMouseLeave={() => setHoverIdx(null)}
+            >
+              <Link
+                href={e.href}
+                className="flex items-center gap-4 p-4 pl-5 h-14"
               >
-                <Link
-                  href={e.href}
-                  className="flex items-center gap-4 p-4 pl-5 text-gray-200 h-14"
-                >
-                  <FontAwesomeIcon icon={e.icon} className="w-4" />
-                  <p>{e.title}</p>
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+                <FontAwesomeIcon icon={e.icon} className="w-4" />
+                <p>{e.title}</p>
+              </Link>
+            </List>
+          )
+        })}
+      </ul>
       <div className="w-full p-3">
         <button
           className="flex items-center justify-center w-full gap-3 p-3 text-gray-200 rounded bg-zinc-600 hover:bg-zinc-500"
@@ -81,6 +80,10 @@ export default function Sidebar() {
     </nav>
   )
 }
+
+const List = styled.li<{ selected: boolean }>((props) => ({
+  color: props.selected ? '#c084fc' : '#e4e4e7',
+}))
 
 const navList = [
   {
