87da29c01e74b55d78dbebbfd7b577dc279582df
pokycookie <0cookieboy0@gmail.com>
2023-08-30 16:29:00 +0900

    fix: toggle animation bug

--- app/components/ui/toggleBtn.tsx
+++ app/components/ui/toggleBtn.tsx
@@ -14,15 +14,12 @@ export default function ToggleBtn(props: IProps) {
 
   return (
     <motion.button
-      className="flex items-center w-10 h-5 p-1 border rounded-full bg-zinc-900 data-[on=true]:justify-end justify-start"
+      className="flex relative items-center w-10 h-5 p-1 border rounded-full bg-zinc-900 data-[on=true]:justify-end justify-start"
       data-on={props.on}
       onClick={clickHandler}
       animate={{ borderColor: props.on ? '#c084fc' : '#3f3f46' }}
     >
-      <motion.div
-        className="h-full bg-purple-400 rounded-full aspect-square"
-        layout
-      ></motion.div>
+      <motion.div className="absolute h-3 bg-purple-400 rounded-full aspect-square"></motion.div>
     </motion.button>
   )
 }
